3a08cf5a5d6f6155e4badc3f008e8f2771777e52
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-13 11:24:25 +0300

    first commit

---
+++ starter/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "please provide email"],
+    validate: {
+        validator: validator.isEmail,
+        message:'Please provide email',
+    },
+  },
+   password: {
+    type: String,
+    required: [true, "please provide password"],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+  },
+});
+
+// password hashing
+UserSchema.pre('save', async function () {
+  console.log('hey there')
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
+
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch; 
+}
+module.exports = mongoose.model('User', UserSchema);
