486d06df1f247a21e24eb5bf1741be362780f3e0
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-14 07:06:20 +0300

    add delete

--- starter/controllers/productController.js
+++ starter/controllers/productController.js
@@ -13,13 +13,27 @@ const getAllProducts = async (req, res) => {
     res.status(StatusCodes.OK).json({products, count: products.length});
 }
 const getSingleProduct = async (req, res) => {
-    res.send('get single product');
+    const {id: productId} = req.params;
+    const product = await Product.find({_id: productId});
+    if(!product) {
+        throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+    }
+    res.status(StatusCodes.OK).json({product})
 }
 const updateProduct = async (req, res) => {
-    res.send('update product');
+    const {id:productId} = req.params;
+    const product = await Product.findOneAndUpdate({_id: productId},req.body,{
+        new: true, 
+        runValidators: true,
+    });
+    if(!product) {
+        throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+    }
+    res.status(StatusCodes.OK).json({product})
 }
 const deleteProduct = async (req, res) => {
-    res.send('delete product');
+   const {id:productId} = req.params;
+   const product = await Product.findOneAndDelete({_id: productId})
 }
 const uploadImage = async (req, res) => {
     res.send('upload image');
