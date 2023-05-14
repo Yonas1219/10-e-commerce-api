ed91a32e62e428a9f1a49a8f54323860674cc6a2
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-14 04:27:30 +0300

    change on user

--- starter/models/User.js
+++ starter/models/User.js
@@ -32,7 +32,9 @@ const UserSchema = new mongoose.Schema({
 
 // password hashing
 UserSchema.pre('save', async function () {
-  console.log('hello there')
+  // console.log(this.modifiedPath());
+  // console.log(this.isModified('name'));
+  if(!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 })
