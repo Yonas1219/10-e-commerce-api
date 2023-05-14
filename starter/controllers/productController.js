33cfd7f536f6644e31b5e35793ad53bd163ca927
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-14 07:09:46 +0300

    some changes

--- starter/controllers/productController.js
+++ starter/controllers/productController.js
@@ -33,7 +33,13 @@ const updateProduct = async (req, res) => {
 }
 const deleteProduct = async (req, res) => {
    const {id:productId} = req.params;
-   const product = await Product.findOneAndDelete({_id: productId})
+   const product = await Product.findOneAndDelete({_id: productId});
+   if(!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+}
+await product.remove();
+res.status(StatusCodes.OK).json({msg: 'success! product removed'})
+
 }
 const uploadImage = async (req, res) => {
     res.send('upload image');
