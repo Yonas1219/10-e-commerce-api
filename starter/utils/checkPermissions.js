481dea6751d3d14d9ec05f0514c0ac612a3c14ca
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-14 04:35:42 +0300

    adding userChecking

---
+++ starter/utils/checkPermissions.js
@@ -0,0 +1,8 @@
+const CustomError = require('../errors')
+const checkPermissions = (requestUser, resourceUserId) => {
+    console.log(requestUser);
+    console.log(resourceUserId);
+    console.log(typeof resourceUserId );
+}
+
+module.exports = checkPermissions;
