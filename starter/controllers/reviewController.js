1354e4f8967bd1e1eb18e0f5b04095b5d126638e
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-20 11:37:07 +0300

    adding controller

--- starter/controllers/reviewController.js
+++ starter/controllers/reviewController.js
@@ -1,23 +1,30 @@
+const Review = require("../models/Review");
+const Product = require("../models/Product");
+
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const { checkPermissions } = require("../utils");
+
 const createReview = async (req, res) => {
-    res.send('create review');
-}
+  res.send("create review");
+};
 const getAllReviews = async (req, res) => {
-    res.send('get all review');
-}
+  res.send("get all review");
+};
 const getSingleReview = async (req, res) => {
-    res.send('get single review');
-}
+  res.send("get single review");
+};
 const updateReview = async (req, res) => {
-    res.send('update review');
-}
+  res.send("update review");
+};
 const deleteReview = async (req, res) => {
-    res.send('delete review');
-}
+  res.send("delete review");
+};
 
 module.exports = {
-    createReview,
-    getAllReviews,
-    getSingleReview,
-    updateReview,
-    deleteReview,
+  createReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+  deleteReview,
 };
