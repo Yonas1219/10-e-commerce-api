e6dc94abe4f1532848e22f573de7c085bd5cb39e
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-20 13:07:00 +0300

    adding getSingleProductReviews

--- starter/controllers/reviewController.js
+++ starter/controllers/reviewController.js
@@ -68,10 +68,17 @@ const deleteReview = async (req, res) => {
   res.status(StatusCodes.OK).json({review});
 };
 
+const getSingleProductReviews = async(req, res) => {
+  const {id:productId} = req.params;
+  const reviews = await Review.find({product: productId})
+  res.status(StatusCodes.OK).json({reviews, count: reviews.length})
+}
+
 module.exports = {
   createReview,
   getAllReviews,
   getSingleReview,
   updateReview,
   deleteReview,
+  getSingleProductReviews,
 };
