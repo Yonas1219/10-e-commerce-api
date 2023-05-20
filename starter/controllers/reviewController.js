d85d00ada382d79b39bf13016fddc4ec1d972c99
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-20 10:33:20 +0300

    adding review controller

---
+++ starter/controllers/reviewController.js
@@ -0,0 +1,15 @@
+const createReview = async (req, res) => {
+    res.send('create review');
+}
+const getAllReviews = async (req, res) => {
+    res.send('create review');
+}
+const getSingleReview = async (req, res) => {
+    res.send('create review');
+}
+const updateReview = async (req, res) => {
+    res.send('create review');
+}
+const deleteReview = async (req, res) => {
+    res.send('create review');
+}
