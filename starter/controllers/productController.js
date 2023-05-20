b92b1d88e8974b84b1a2858262c1850cda7404a5
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-20 10:11:18 +0300

    image path

--- starter/controllers/productController.js
+++ starter/controllers/productController.js
@@ -1,64 +1,75 @@
-const Product = require('../models/Product');
-const {StatusCodes} = require('http-status-codes');
-const CustomError = require('../errors');
+const Product = require("../models/Product");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const path = require("path");
 
 const createProduct = async (req, res) => {
-    req.body.user = req.user.userId;
-    const product = await Product.create(req.body);
-    res.status(StatusCodes.CREATED).json({product});
-}
+  req.body.user = req.user.userId;
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json({ product });
+};
 const getAllProducts = async (req, res) => {
-    const products = await Product.find({}); 
-     
-    res.status(StatusCodes.OK).json({products, count: products.length});
-}
-const getSingleProduct = async (req, res) => {
-    const {id: productId} = req.params;
-    const product = await Product.find({_id: productId});
-    if(!product) {
-        throw new CustomError.NotFoundError(`No product with id: ${productId}`);
-    }
-    res.status(StatusCodes.OK).json({product})
-}
-const updateProduct = async (req, res) => {
-    const {id:productId} = req.params;
-    const product = await Product.findOneAndUpdate({_id: productId},req.body,{
-        new: true, 
-        runValidators: true,
-    });
-    if(!product) {
-        throw new CustomError.NotFoundError(`No product with id: ${productId}`);
-    }
-    res.status(StatusCodes.OK).json({product})
-}
-const deleteProduct = async (req, res) => {
-   const {id:productId} = req.params;
-   const product = await Product.findOneAndDelete({_id: productId});
-   if(!product) {
-    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
-}
-await product.remove();
-res.status(StatusCodes.OK).json({msg: 'success! product removed'})
+  const products = await Product.find({});
 
-}
+  res.status(StatusCodes.OK).json({ products, count: products.length });
+};
+const getSingleProduct = async (req, res) => {
+  const { id: productId } = req.params;
+  const product = await Product.find({ _id: productId });
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+  res.status(StatusCodes.OK).json({ product });
+};
+const updateProduct = async (req, res) => {
+  const { id: productId } = req.params;
+  const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+  res.status(StatusCodes.OK).json({ product });
+};
+const deleteProduct = async (req, res) => {
+  const { id: productId } = req.params;
+  const product = await Product.findOneAndDelete({ _id: productId });
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+  await product.remove();
+  res.status(StatusCodes.OK).json({ msg: "success! product removed" });
+};
 const uploadImage = async (req, res) => {
-    if(!req.files){
-        throw new CustomError.BadRequestError('no file uploaded')
-    }
-    
-    const productImage = req.files.image;
-    if(!productImage.mimetype.startsWith('image')){
-        
-        throw new CustomError.BadRequestError('please uploaded image')
-    }
-    res.send('upload image');
-}
+  if (!req.files) {
+    throw new CustomError.BadRequestError("no file uploaded");
+  }
+
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("please uploaded image");
+  }
+
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError("image size is too large");
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads." + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+  res.status(StatusCodes.OK).json({image:`/uploads/${productImage.name}`});
+};
 
 module.exports = {
-    createProduct,
-    getAllProducts,
-    getSingleProduct,
-    updateProduct,
-    deleteProduct,
-    uploadImage,
-}
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
+};
