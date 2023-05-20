f61c1781f8831f452366d7ed1747afe62552640f
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-20 10:45:19 +0300

    some refactoring

--- starter/app.js
+++ starter/app.js
@@ -15,6 +15,8 @@ const connectDB = require("./db/connect");
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
 const productRouter = require('./routes/productRoutes')
+const reviewRouter = require('./routes/reviewRoutes')
+
 // middleware
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -43,6 +45,7 @@ app.get('/api/v1/auth', (req, res) => {
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
+app.use('/api/v1/reviews', reviewRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
