45cc99cf684427684f75dcb75c2d73e92bd42980
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-20 10:22:47 +0300

    adding review schema

---
+++ starter/models/Review.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "please provide rating"],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "please provide title"],
+      maxLength: 100,
+    },
+    comment: {
+      type: String,
+      required: [true, "please provide text"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Review', ReviewSchema);
