33ef7e55fd7d7bd7d602f90af9705709c0358787
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-28 11:37:55 +0300

    order model

---
+++ starter/models/Order.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cartItems: [],
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentIntentId: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Order', OrderSchema);
