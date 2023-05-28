b3b45c1a21295858832777e2804a9b016519c804
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-28 13:28:08 +0300

    single order

--- starter/controllers/ordercontroller.js
+++ starter/controllers/ordercontroller.js
@@ -67,7 +67,13 @@ const getAllOrders = async (req, res) => {
     res.status(StatusCodes.OK).json({ orders, count: orders.length });
   };
 const getSingleOrder = async (req, res) => {
-  res.send("get Single order");
+    const { id: orderId } = req.params;
+    const order = await Order.findOne({ _id: orderId });
+    if (!order) {
+      throw new CustomError.NotFoundError(`No order with id : ${orderId}`);
+    }
+    checkPermissions(req.user, order.user);
+    res.status(StatusCodes.OK).json({ order });
 };
 const getCurrentUserOrders = async (req, res) => {
   res.send("get current users order");
