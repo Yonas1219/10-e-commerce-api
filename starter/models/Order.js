19f60dcb9abbeb33f4aa5b69bc3a339a4ebf3867
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-28 11:46:04 +0300

    adding Schema

--- starter/models/Order.js
+++ starter/models/Order.js
@@ -1,5 +1,16 @@
 const mongoose = require("mongoose");
 
+const SingleCartItemSchema = mongoose.Schema({
+    name:{type:String, required:true},
+    image:{type:String, required:true},
+    price:{type:Number, required:true},
+    amount:{type:Number, required:true},
+    product: {
+        type:mongoose.Schema.ObjectId,
+        ref:"Product",
+        required:true,
+    },
+});
 const OrderSchema = mongoose.Schema(
   {
     tax: {
