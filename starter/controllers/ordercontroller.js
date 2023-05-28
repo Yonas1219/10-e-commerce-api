4322a919854c09020766e60f98ecaa481d3f8897
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-28 12:52:41 +0300

    changes

--- starter/controllers/ordercontroller.js
+++ starter/controllers/ordercontroller.js
@@ -1,40 +1,64 @@
-const Order = require('../models/Order');
-const Product = require('../models/Product');
+const Order = require("../models/Order");
+const Product = require("../models/Product");
 
-const {StatusCodes} = require('http-status-codes');
-const CustomError = require('../errors');
-const {checkPermission} = require('../utils');
-
-
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const { checkPermission } = require("../utils");
 
 const createOrder = async (req, res) => {
-const { item: cartItems, tax, shippingFee } = req.body;
+  const { items: cartItems, tax, shippingFee } = req.body;
 
-if(!cartItems || cartItems.length < 1){
-    throw new CustomError.BadRequestError('No cart items provided')
-}
-if(!tax || !shippingFee){
-    throw new CustomError.BadRequestError('please provide tax and shipping fee')
-}
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomError.BadRequestError("No cart items provided");
+  }
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError(
+      "please provide tax and shipping fee"
+    );
+  }
+  let orderItems = [];
+  let subtotal = 0;
+
+  for(const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(`No product with id ${item.product}`);
+    }
+    const {name, price, image, _id } = dbProduct;
+    const getSingleOrder = {
+        amount: item.amount,
+        name,
+        price,
+        image,
+        product: _id
+    };
+    // add items to order list
+    orderItems = [...orderItems, SingleOrderItem]
+    // calc subtotal
+    subtotal += item.amount * price
+  }
+
+  console.log(orderItems);
+  console.log(subtotal);
+  res.send("hello");
 };
 const getAllOrders = async (req, res) => {
-res.send('get all order')
-}
+  res.send("get all order");
+};
 const getSingleOrder = async (req, res) => {
-res.send('get Single order')
-}
+  res.send("get Single order");
+};
 const getCurrentUserOrders = async (req, res) => {
-res.send('get current users order')
-}
+  res.send("get current users order");
+};
 const updateOrder = async (req, res) => {
-res.send('update order')
-}
+  res.send("update order");
+};
 
 module.exports = {
-    getAllOrders,
-    getSingleOrder,
-    getCurrentUserOrders,
-    createOrder,
-    updateOrder,
-
-}
+  getAllOrders,
+  getSingleOrder,
+  getCurrentUserOrders,
+  createOrder,
+  updateOrder,
+};
