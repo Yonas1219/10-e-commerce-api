4075baa3b890d3de162cd92c48cf528fc787259a
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-28 12:34:04 +0300

    refactor controller

--- starter/controllers/ordercontroller.js
+++ starter/controllers/ordercontroller.js
@@ -1,6 +1,22 @@
+const Order = require('../models/Order');
+const Product = require('../models/Product');
+
+const {StatusCodes} = require('http-status-codes');
+const CustomError = require('../errors');
+const {checkPermission} = require('../utils');
+
+
+
 const createOrder = async (req, res) => {
-res.send('create order')
+const { item: cartItems, tax, shippingFee } = req.body;
+
+if(!cartItems || cartItems.length < 1){
+    throw new CustomError.BadRequestError('No cart items provided')
 }
+if(!tax || !shippingFee){
+    throw new CustomError.BadRequestError('please provide tax and shipping fee')
+}
+};
 const getAllOrders = async (req, res) => {
 res.send('get all order')
 }
