17f6187ef00346ebd393f5f23231db94a838b360
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-28 12:02:11 +0300

    changes

---
+++ starter/routes/orderRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
+
+const {
+  getAllOrders,
+  getSingleOrder,
+  getCurrentUserOrders,
+  createOrder,
+  updateOrder,
+} = require('../controllers/orderController');
